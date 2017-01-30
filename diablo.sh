11a2f85429a492fbb91292009a89abe263c4fb41
suboxide <suboxide@gmail.com>
2017-01-30 13:20:23 +0100

    zsh

---
+++ diablo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#exec primusrun wine "/home/sub/Diablo/Battle.net/Battle.net.exe"
+#exec optirun -b primus wine "/home/sub/Diablo 3/Battle.net/Battle.net Launcher.exe"
+#exec primusrun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec optirun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec optirun -b primus /opt/cxoffice/bin/wine -bottle "Diablo_III" --cx-app "Battle.net Launcher.exe"
+#exec optirun -b primus /opt/cxoffice/bin/wine -bottle "Diablo_III" --cx-app battle.net.exe
+#exec primusrun /opt/cxoffice/bin/wine -bottle "Diablo_III" --cx-app "Diablo III Launcher.exe"
+#exec optiprime wine /home/sub/Downloads/dd.exe  
+exec optiprime wine /home/sub/Diablo/Battle.net/Battle.net.exe
+
+
+exit "0"
+
+	
---
+++ word.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#exec optirun -b primus wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec primusrun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec optirun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+
+exec optiprime /opt/cxoffice/bin/wine -bottle "Microsoft Office 2013" --cx-app winword.exe
+
+
+
+exit "0"
+

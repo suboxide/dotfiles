317bd1cf3ce365844d6d859ddbc7f03776b8514a
suboxide <suboxide@gmail.com>
2017-02-01 14:21:33 +0100

    last test

--- gw2.sh
+++ gw2.sh
@@ -7,5 +7,6 @@ exec optiprime wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
 #exec primusrun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
 #exec optirun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
 
+
 exit "0"
 
---
+++ steam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#exec optiprime wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+
+#exec optirun -b primus wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec primusrun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+#exec optirun wine /home/sub/GW2/Gw2-64.exe -dxs9ingle
+
+exec sudo haguichi &                                                                                              ⏎
+exec optiprime steam -silent &
+
+
+exit "0"

36536fe1dcd47a020762fb9e7f1ccc12b0c4efd8
suboxide <suboxide@gmail.com>
2017-02-03 09:15:24 +0100

    wallpaper

---
+++ funny.sh
@@ -0,0 +1,41 @@
+curl -L git.io/unix
+
+curl -L git.io/pizzza
+
+curl -L git.io/vburger
+
+curl -L git.io/burger
+
+curl -L git.io/rice
+
+curl -L git.io/waffles
+
+curl -L git.io/poptart
+
+curl -L git.io/pancakes
+
+curl -L git.io/taco
+
+curl -L git.io/metroid
+
+curl -L git.io/troid
+
+curl -L git.io/supermetroid
+
+curl -L http://0x0.st/8w
+
+curl -L http://0x0.st/8x
+
+curl pub.iotek.org/p/qNFTDms
+
+curl 0w.nz/fsociety.ascii
+
+curl 0w.nz/x0-toxic-skull.txt
+
+curl 0w.nz/x0-ryu.txt
+
+curl 0w.nz/x0-link.txt
+
+curl 0w.nz/x0-impure-xero.txt
+
+curl 0w.nz/x0-city.txt
